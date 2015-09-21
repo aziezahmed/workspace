84475694547f34ac629d3d6cef246c6e834141c1
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2015-09-21 19:57:52 +0100

    correcting the attribute names for the new db

--- nodejs/database_test.js
+++ nodejs/database_test.js
@@ -3,7 +3,7 @@ var file = "../sql/bookmarks.db";
 var db = new sqlite3.Database(file);  
 db.all("SELECT * FROM bookmarks", function(err, rows) {  
         rows.forEach(function (row) {  
-            console.log(row.Title, row.Url);  
+            console.log(row.title, row.url);  
         })  
     });   
 db.close(); 
