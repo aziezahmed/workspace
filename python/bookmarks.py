e923ab8f28a289e5c8b22395378292385ef86652
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2015-09-22 12:24:49 +0100

    Create a python script to interact with the database

---
+++ python/bookmarks.py
@@ -0,0 +1,45 @@
+import sqlite3
+import sys
+
+connection = None
+connection = sqlite3.connect('../sql/bookmarks.db')
+
+cursor = connection.cursor()
+
+print("Bookmarks")
+print("1 - list all bookmarks")
+print("2 - add a bookmarks")
+print("3 - delete a bookmark")
+print("4 - exit")
+
+option = 0;
+
+while (option != 4):
+    option = input("Select and option: ")
+
+    if option == 1:
+
+        cursor.execute('SELECT * FROM bookmarks')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row[1] + " " + row[2])
+
+    if option == 2:
+
+        title = raw_input("Enter a title: ")
+        url = raw_input("Enter a url: ")
+
+        cursor.execute('INSERT INTO bookmarks (title, url) VALUES ("' + title + '", "' + url + '");')
+        connection.commit()
+
+    if option == 3:
+        cursor.execute('SELECT * FROM bookmarks')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(str(row[0]) + " - " + row[1] + " " + row[2])
+
+        key = raw_input("Enter a number: ")
+        cursor.execute('DELETE FROM bookmarks WHERE id=' + key + ';')
+        connection.commit()
+
+connection.close()
