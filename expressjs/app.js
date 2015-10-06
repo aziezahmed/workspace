7f9f5b8d0d7c01f85663b5f3629123666d4ab9d6
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2015-10-06 14:43:26 +0100

    Update app.js to include post and delete methods

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,22 +1,43 @@
 var express = require('express');
 var app = express();
-var sqlite3 = require('sqlite3').verbose();  
+var sqlite3 = require('sqlite3').verbose();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
   next();
 });
 
+var file = "../sql/bookmarks.db";
+var db = new sqlite3.Database(file);
+var sqlRequestAllRows = "SELECT * FROM bookmarks";
 app.get('/', function(req, res, next) {
-	
-	var file = "../sql/bookmarks.db";  
-	var db = new sqlite3.Database(file);  
-	db.all("SELECT * FROM bookmarks", function(err, rows) {  
-		res.json(rows);
-	});   
-	db.close(); 
-	
+  db.all(sqlRequestAllRows, function(err, rows) {
+    res.json(rows);
+  });
+});
+
+app.post('/', function (req, res) {
+  var title = req.body.title;
+  var url = req.body.url;
+  var sqlRequest = "INSERT INTO bookmarks (title, url) VALUES ('" + title + "', '" + url + "')";
+  db.run(sqlRequest, function(err) {});
+  db.all(sqlRequestAllRows, function(err, rows) {
+    res.json(rows);
+  });
+});
+
+app.delete('/:id', function (req, res) {
+  var sqlRequest = "DELETE FROM bookmarks WHERE id='" + req.params.id + "'";
+  db.run(sqlRequest,function(err) {});
+  db.all(sqlRequestAllRows, function(err, rows) {
+    res.json(rows);
+  });
 });
 
 var server = app.listen(3000, function () {
